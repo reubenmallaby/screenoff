3f457a2c98d6d20ec295a0c6c47911f6eab6be41
Reuben Mallaby <reuben@mallaby.me>
2020-02-11 13:26:40 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ScreenOff
+{
+    class Program
+    {        
+        [DllImport("user32.dll")]
+        static extern IntPtr PostMessage(int hWnd, int msg, int wParam, int lParam);
+
+        private const int WmSyscommand = 0x0112;
+        private const int ScMonitorpower = 0xF170;
+        private const int MonitorShutoff = 2;
+        
+        static void Main(string[] args)
+        {
+            PostMessage(-1, WmSyscommand, ScMonitorpower, MonitorShutoff);
+        }
+    }
+}
